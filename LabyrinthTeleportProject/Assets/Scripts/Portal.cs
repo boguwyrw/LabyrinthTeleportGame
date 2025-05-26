f0b18e7486d7b7c3be42c16ae35833fa404b1be6
Bogumil Wyrwal <b.wyrwal@wp.pl>
2025-05-26 18:20:01 +0200

    Teleportation has been finished

--- LabyrinthTeleportProject/Assets/Scripts/Portal.cs
+++ LabyrinthTeleportProject/Assets/Scripts/Portal.cs
@@ -7,12 +7,33 @@ public class Portal : MonoBehaviour
     [SerializeField] Portal otherPortal;
     [SerializeField] Material material;
 
+    [SerializeField] private GameObject player;
+    [SerializeField] private PortalTeleport portalTeleport;
+
+    [SerializeField] private PortalCamera portalCamera;
+
     public Camera myCamera;
 
     public Transform renderSurface;
     public Transform portalCollider;
 
-    
+    private void Awake()
+    {
+        portalTeleport.player = player.transform;
+        portalTeleport.receiver = otherPortal.portalCollider;
 
+        renderSurface.GetComponent<Renderer>().material = Instantiate(material);
+        if (myCamera.targetTexture != null)
+        {
+            myCamera.targetTexture.Release();
+        }
 
+        myCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+    }
+
+    private void Start()
+    {
+        renderSurface.GetComponent<Renderer>().material.mainTexture =
+            otherPortal.GetComponent<Portal>().myCamera.targetTexture;
+    }
 }
